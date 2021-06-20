c4a3f0b6dae05a97261bd78d51010d249f154c90
Steve Holland <sdh4@iastate.edu>
2021-06-20 07:37:29 -0500

    main_thread: Missing import; discussion of parallelism issues

--- dataguzzler_python/main_thread.py
+++ dataguzzler_python/main_thread.py
@@ -3,6 +3,7 @@ import os
 import signal
 import queue
 import atexit
+import traceback
 
 from .dgpy import SimpleContext,InitContext,PushThreadContext,PopThreadContext
 from .mainloop import do_systemexit
@@ -26,6 +27,46 @@ def queue_to_run_in_main_thread(callable, *args,dgpy_compatible_context=None,**k
     is run in that context. 
 
     (The callable is free to push and pop thread contexts as needed)
+
+    Be aware that if you pass this function an OpaqueWrapper, that will
+    trigger a context change to the wrapped context when called, and
+    if your function is an infinite loop it will then monopolize the
+    context, except when calling out to functions/methods in other 
+    contexts. 
+
+    One handy trick may be to intentionally put your main loop in 
+    your module context (usually with PushThreadContext(module_instance)), 
+    but put the waiting function in a secondary
+    context but with some sort of trigger (perhaps functionality for
+    this might be built into dgpy.Module in the future?) to terminate 
+    the wait whenever an outside method call is performed. That way
+    even inside the main loop and all methods (except for the wait), 
+    all code will be executed with the module's lock held, and 
+    thread safety worries are dramatically reduced. 
+
+    Another related approach would be some future hook in 
+    dgpy.Module to delegate method calls to the main loop, such that
+    all code executes in that main loop context. The problem with
+    this approach is that it is only safe to make direct synchronous
+    calls to outside modules if they can't possibly try to call 
+    back to your module, which is hard to guarantee (occupation of 
+    the main loop counts as a lock, which is not dropped when you 
+    make a method call to another module. It must be considered in 
+    the locking order. As long as it is first in the order you are 
+    OK but obviously if an indirect call gets somehow back to your
+    module, it will try to delegate to the main loop, which is then
+    occupied. A possible workaround for this would be to detect
+    the case of a recursive call in the main loop context, and 
+    execute the method directly rather than delegating. 
+
+    If there's a question about what context any given bit of code
+    is executing in, the following lines will print that to 
+    standard error as a tuple: 
+        from dataguzzler_python.context import FormatCurContext
+        sys.stderr.write(FormatCurContext()+"\n")
+    The second element of the tuple is the module name, dgpy_main_thread,
+    or similar. 
+
     """
     main_thread_queue.put((callable,args,dgpy_compatible_context,kwargs))
     pass
